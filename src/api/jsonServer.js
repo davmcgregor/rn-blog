c61cfe827b0b6e8f42b1417daf5b9d5b4524c80e
David McGregor <davidjohnmcgregor@gmail.com>
2020-11-09 22:53:51 +0000

    Set up getBlogPosts action

---
+++ src/api/jsonServer.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'http://0d6e02b2717d.ngrok.io',
+});
+
+
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,8 +1,10 @@
-import React from 'react';
 import createDataContext from './createDataContext';
+import jsonServer from '../api/jsonServer';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'get_blogposts':
+      return action.payload;
     case 'add_blogpost':
       return [
         ...state,
@@ -21,6 +23,14 @@ const blogReducer = (state, action) => {
     default:
       return state;
   }
+};
+
+const getBlogPosts = (dispatch) => {
+  return async () => {
+    const response = await jsonServer.get('/blogposts');
+
+    dispatch({ type: 'get_blogposts', payload: response.data });
+  };
 };
 
 const addBlogPost = (dispatch) => {
@@ -52,6 +62,6 @@ const editBlogPost = (dispatch) => {
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost, editBlogPost },
-  [{ title: 'TEST POST', content: 'TEST CONTENT', id: 1 }]
+  { addBlogPost, deleteBlogPost, editBlogPost, getBlogPosts },
+  []
 );
