70bd36c2ed00593f6e600a1c0770e53317961def
David McGregor <davidjohnmcgregor@gmail.com>
2020-11-10 18:04:30 +0000

    Add refetch on navigation to IndexScreen

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -5,15 +5,15 @@ const blogReducer = (state, action) => {
   switch (action.type) {
     case 'get_blogposts':
       return action.payload;
-    case 'add_blogpost':
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 99999),
-          title: action.payload.title,
-          content: action.payload.content,
-        },
-      ];
+    // case 'add_blogpost':
+    //   return [
+    //     ...state,
+    //     {
+    //       id: Math.floor(Math.random() * 99999),
+    //       title: action.payload.title,
+    //       content: action.payload.content,
+    //     },
+    //   ];
     case 'edit_blogpost':
       return state.map((blogPost) => {
         return blogPost.id === action.payload.id ? action.payload : blogPost;
@@ -34,8 +34,9 @@ const getBlogPosts = (dispatch) => {
 };
 
 const addBlogPost = (dispatch) => {
-  return (title, content, callback) => {
-    dispatch({ type: 'add_blogpost', payload: { title, content } });
+  return async (title, content, callback) => {
+    await jsonServer.post('/blogposts', { title, content });
+    // dispatch({ type: 'add_blogpost', payload: { title, content } });
     if (callback) {
       callback();
     }
